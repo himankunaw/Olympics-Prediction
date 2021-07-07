d58a3a63c643464c81a16db08a4f7455237b4d5b
Himanshu Kumar <81146917+himankunaw@users.noreply.github.com>
2021-07-07 22:40:46 +0530

    Features used from WDIData

---
+++ Code/features.py
@@ -0,0 +1,24 @@
+desired_indicators = \
+    ['NY.GDP.MKTP.KD.ZG',\
+     'NY.GDP.PCAP.KD',\
+         'NY.GDP.MKTP.KD',\
+             'NY.GDP.MKTP.KD',\
+                 'SP.POP.TOTL',\
+                     'SP.POP.TOTL',\
+                         'AG.SRF.TOTL.K2',\
+                             'SP.POP.GROW']
+        
+field_names = \
+    ['GDP_Growth',\
+     'GDP_Per_Capita',\
+         'GDP',\
+             'GDP_Normalized',\
+                 'Pop',\
+                     'Pop_Normalized',\
+                         'Area',\
+                             'Pop_Growth']
+    
+year_begin = 1988
+year_end = 2016
+
+validation_year = 2016
